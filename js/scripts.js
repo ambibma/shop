6f22f950ad9bafddfe93fb7b5083c1eb29110aca
ambi hidalgo <ambio.pk@gmail.com>
2022-11-02 21:02:59 -0700

    init commit

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+function purchase(event) {
+event.preventDefault();
+const watch = document.getElementById("watch").value;
+const necklace = document.getElementById("necklace").value;
+const earrings = document.getElementById("earrings").value;
+
+if (watch === "watch") {
+  document.querySelector("span#total").innertext = "1,000.00"
+  console.log(watch);
+} else 
+  
+
+window.addEventListener("load", function() {
+  let form = document.getElementById("cart");
+  let button = document.getElementsByName("submit");
+  
+  
+})
